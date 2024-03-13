0fab8573fdf81d8877a592279e88426c7553796a
Aaron Imbrock <aimbrock@gmail.com>
2024-03-12 22:27:50 -0500

    Update kubeadm-create-minimal-cluster.sh

--- kubeadm-create-minimal-cluster.sh
+++ kubeadm-create-minimal-cluster.sh
@@ -1,7 +1,5 @@
-
-
-if [ $(hostname) == "controlplane" ]:
+if [[ $(hostname) == "controlplane" ]]; then
   sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.0.121
-else:
+else
   echo "Not Yet Implemented"
 fi
